07bb295ba0a753d8fb0642dd902d372a705df5e1
Bo <1442983474@qq.com>
2018-04-16 14:18:24 +0800

    login success

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/help',    to: 'static_pages#help'
+  get '/help',     to: 'static_pages#help'
 
-  get '/about',   to: 'static_pages#about'
+  get '/about',    to: 'static_pages#about'
 
-  get '/contact', to: 'static_pages#contact'
+  get '/contact',  to: 'static_pages#contact'
 
-  get '/signup',  to: 'users#new'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+
+  resources :users
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
