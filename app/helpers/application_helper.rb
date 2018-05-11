febe72cfad1ad1a73e2546f4812b6c0d8463271c
trh <1442983474@qq.com>
2018-05-12 00:34:59 +0800

    中文版

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     # 根据所在的页面返回完整的标题
     def full_title(page_title = '')
-        base_title = "Petrochemical Corrosion App"
+        base_title = "石化腐蚀数据管理分析系统"
         if page_title.empty?
             base_title
         else
